b0d014830a6cc9c3a5b1bfd866d480de979c8b92
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-31 09:50:32 +0530

    [Akanksha] added  passward in one numeric

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC_7_PasswardIn1Numeric
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your passward with 1 Numeric: ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][0-9][a-z]{6}$");
+
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("Password is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Passward is " + false);
+                }
+            }
+        }
+    }
+}
