85da95937fb376bfa20b463ad1951a17419e91fb
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-31 09:26:43 +0530

    [Akanksha] added user first name

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC_1_UserFirstName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter your First Name ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][a-z]{2,}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("your First name is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your First Name is " + false);
+                    Console.WriteLine("First Name Should Contain Only Four Character");
+                }
+            }
+        }
+    }
+}
