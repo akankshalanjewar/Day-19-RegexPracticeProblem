b2f1a323abe754368be80d11fb744ec238baf941
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-31 09:42:32 +0530

    [Akanksha] added user  passward

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC_5_UserPassward
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Password : ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Za-z0-9]{8}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("Your Password Is :" + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your Password is " + false);
+                }
+            }
+        }
+    }
+}
+
+        
+    
+
