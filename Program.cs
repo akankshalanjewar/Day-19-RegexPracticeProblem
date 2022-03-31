5155aee50bd985cabcbd1e41e0a42820ddec5fd7
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-31 09:56:39 +0530

    [Akanksha] added passward has one character

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC_8_PasswardHas1character
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password with one Upper,one numberic,one special character: ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex(@"^[A-Z][0-9](|_|.|-|~|@|)[a-z]{5}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("Password is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Password is " + false);
+                }
+            }
+        }
+    }
+}
