7b142224c70c4f0cb387eab4a02a96bfe51d0a04
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-31 09:39:05 +0530

    [Akanksha] added user mobile Number

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC_4_userMobileNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Mobile No:");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^91 [1-9][0-9]{9}$");
+
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
